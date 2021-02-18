8c118ef27bdaf5b1b0e96ca3df3cf84d63c8d6f9
Daan <daanroth@protonmail.com>
2021-02-18 17:43:58 +0100

    huidige status

---
+++ Opdracht0/gnuifier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string gnuifier(string argument) {
+  return "GNU/" + argument;
+}
+
+int main( int argc, char *argv[] ) {
+   if ( argc != 2 ) {
+     cout << "Maar 1 argument nodig" << endl;
+   } else {
+     cout << gnuifier(argv[1]) << endl;
+   }
+   return 0;
+}
