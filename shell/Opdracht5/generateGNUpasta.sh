3142fc9ab556e7b7a7aba54dd947dc382eca1afc
Appenzeill <daanroth@protonmail.com>
2021-03-18 17:34:34 +0100

    Opdracht 5 iets mooier gemaakt

--- shell/Opdracht5/generateGNUpasta.sh
+++ shell/Opdracht5/generateGNUpasta.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # ooit gemakkelijk je eigen versie van de GNU/Linux copypasta willen maken?
-# hier is je kans. geef een argument mee voor een woord die veranders moet worden.
+# hier is je kans. geef een argument mee om Linux te veranderen naar keuze.
+# Zonder arguments zal de default copy pasta worden uitgevoerd.
 
-ARGUMENT=$1
+ARGUMENT1=$1
+ARGUMENT2=$2
 
-echo "I’d just like to interject for a moment. What you’re refering to as $ARGUMENT, is in fact, GNU/$ARGUMENT, or as I’ve recently taken to calling it, GNU plus $ARGUMENT."
-echo "$ARGUMENT is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+if [ "$ARGUMENT1" == "" ]; then
+    ARGUMENT1="GNU"
+fi
+if [ "$ARGUMENT2" == "" ]; then
+    ARGUMENT2="Linux"
+fi
+
+echo "I’d just like to interject for a moment. What you’re refering to as $ARGUMENT2, is in fact, $ARGUMENT1/$ARGUMENT2, or as I’ve recently taken to calling it, $ARGUMENT1 plus $ARGUMENT2."
+echo "$ARGUMENT1 is not an operating system unto itself, but rather another free component of a fully functioning $ARGUMENT2 system made useful by the $ARGUMENT1 corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
