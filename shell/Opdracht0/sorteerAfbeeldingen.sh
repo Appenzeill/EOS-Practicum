dec1f152956111e1ea89f4a026dc85db50993d27
Appenzeill <daanroth@protonmail.com>
2021-03-18 22:59:23 +0100

    Opdracht 0 werkt nu

--- shell/Opdracht0/sorteerAfbeeldingen.sh
+++ shell/Opdracht0/sorteerAfbeeldingen.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-mkdir Afbeeldingen
-mv *.png Afbeeldingen/
+mkdir "Afbeeldingen"
+if [ ! -d "Afbeeldingen" ]; then
+  mkdir -p "Afbeeldingen";
+fi
+
+find $1 -iname '*.png' -exec mv '{}' Afbeeldingen \;
+find $1 -iname '*.PNG' -exec mv '{}' Afbeeldingen \;
+find $1 -iname '*.JPEG' -exec mv '{}' Afbeeldingen \;
+find $1 -iname '*.jpeg' -exec mv '{}' Afbeeldingen \;
+find $1 -iname '*.jpg' -exec mv '{}' Afbeeldingen \;
+find $1 -iname '*.JPG' -exec mv '{}' Afbeeldingen \;
