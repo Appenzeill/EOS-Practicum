d325215f2073916442d9cce6285be3d3500d0ada
Appenzeill <daanroth@protonmail.com>
2021-03-18 17:18:02 +0100

    Opdracht 5 af, beetje minimaal.

---
+++ shell/Opdracht5/generateGNUpasta.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# ooit gemakkelijk je eigen versie van de GNU/Linux copypasta willen maken?
+# hier is je kans. geef een argument mee voor een woord die veranders moet worden.
+
+ARGUMENT=$1
+
+echo "I’d just like to interject for a moment. What you’re refering to as $ARGUMENT, is in fact, GNU/$ARGUMENT, or as I’ve recently taken to calling it, GNU plus $ARGUMENT."
+echo "$ARGUMENT is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
